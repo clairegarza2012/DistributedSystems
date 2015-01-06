1b9d50420bb63c84f41ef5295056e352e0812e1f
clairegarza2012 <cgarza@student.neumont.edu>
2015-01-06 09:09:42 -0700

    more preperation stuff
    added constructors to the server and client ... added sockets

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/35/60bfd458be95001411c5de3b1c94ebe8
@@ -0,0 +1,14 @@
+package edu.neumont.csc380;
+
+public class Server {
+
+	/* The server should provide simple CRUD operations for a key/value store. 
+	 *  
+	*/
+	
+	public static void main(String[] args){
+		
+	}
+	
+	
+}
--- Exercise1-Simple_Client_Server/src/edu/neumont/csc380/Server.java
+++ Exercise1-Simple_Client_Server/src/edu/neumont/csc380/Server.java
@@ -1,5 +1,9 @@
 package edu.neumont.csc380;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
 	/* The server should provide simple CRUD operations for a key/value store. 
@@ -8,7 +12,18 @@ public class Server {
 	
 	public static void main(String[] args){
 		
+		new Server();
 	}
 	
+	public Server(){
+		try {
+			ServerSocket ss = new ServerSocket(2222); // 2222 is the port
+			Socket socket = ss.accept();
+			
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
